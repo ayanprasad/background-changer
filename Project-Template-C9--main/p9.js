d3da3bc3fe41dd42c5eccb1e8f26174ced917e14
ayanprasad <102283316+ayanprasad@users.noreply.github.com>
2022-05-02 12:20:21 +0530

    Add files via upload

---
+++ Project-Template-C9--main/p9.js
@@ -0,0 +1,63 @@
+
+function setup() {
+  createCanvas(400,400);
+  background(51);
+  box = createSprite(200,200,30,30);
+
+}
+
+function draw() 
+{
+
+  // write the code to change background color 
+  // to red when RIGHT_ARROW is pressed
+  
+  
+
+  if (keyIsDown(LEFT_ARROW)) 
+  {
+    background("blue");
+    
+  }
+ 
+    if (keyIsDown(UP_ARROW)) 
+  {
+    background("yellow");
+   
+  }
+
+  if (keyIsDown(DOWN_ARROW)) 
+  {
+    background("green");
+
+
+
+  drawSprites();
+}
+
+createCanvas(300,300);
+background(41);
+box=createSprite(100,100,10,10)
+
+
+if(keyIsdown(LEFT_ARROW))
+{
+background("blue");
+
+
+if(keyIsdown(DOWN_ARROW))
+}
+background("green");
+
+
+if(keyISdown(UP_ARROW))
+{
+background("yellow");
+
+
+if(keyIsdown(right_arrow))
+}
+background("red");
+
+drawSprites();
+
---
+++ Project-Template-C9--main/sketch.js
@@ -0,0 +1,63 @@
+
+function setup() {
+  createCanvas(400,400);
+  background(51);
+  box = createSprite(200,200,30,30);
+
+}
+
+function draw() 
+{
+
+  // write the code to change background color 
+  // to red when RIGHT_ARROW is pressed
+  
+  
+
+  if (keyIsDown(LEFT_ARROW)) 
+  {
+    background("blue");
+    
+  }
+ 
+    if (keyIsDown(UP_ARROW)) 
+  {
+    background("yellow");
+   
+  }
+
+  if (keyIsDown(DOWN_ARROW)) 
+  {
+    background("green");
+
+
+
+  drawSprites();
+}
+
+createCanvas(300,300);
+background(41);
+box=createSprite(100,100,10,10)
+
+
+if(keydown(LEFT_ARROW))
+{
+background("blue");
+
+
+if(keydown(DOWN_ARROW))
+}
+background("green");
+
+
+if(keydown(UP_ARROW))
+{
+background("yellow");
+
+
+if(keydown(right_arrow))
+}
+background("red");
+
+
+
